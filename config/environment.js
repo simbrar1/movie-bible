c4942db33b43c15835d1dfe7c89e03f7b1cb3cd2
simbrar1 <sim.brar1@hotmail.com>
2019-07-01 12:31:39 +0100

    first build

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 4000
+const port = process.env.PORT || 4000
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/movies2'
 const secret = process.env.SECRET || 'this is a secret'
 
